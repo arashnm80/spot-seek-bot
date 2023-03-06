703caf097369597851f3038cdc2abf1d3c3ec370
Arash Nemat Zadeh <arashnm80@gmail.com>
2023-03-06 02:50:20 +0000

    2023-03-06-0619-pretty stable

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from functions import download, file_list
 from variables import welcome_message, directory, bot_api, database_channel, spotify_song_link_pattern
 from log import log, log_channel_id
 from database import csv_read, csv_append, csv_search, csv_sort
-from spotify import valid_song_link, get_all_song_links, get_song_id
+from spotify import valid_spotify_song_link, get_all_spotify_song_links, get_spotify_song_id
 
 bot = telebot.TeleBot(bot_api)
 
@@ -22,21 +22,30 @@ def start_message(message):
 @bot.message_handler(regexp = spotify_song_link_pattern)
 def get_by_index(message):
     bot.send_message(message.chat.id, "right spotify_song_link_pattern")
-    matches = get_all_song_links(message.text)
+    matches = get_all_spotify_song_links(message.text)
     if matches:
         # download every link:
         for link in matches:
-            download(link)
-        # upload to telegram and delete from hard drive:
-        for file in file_list(directory):
-            # first send to database_channel:
-            audio = open(directory + file, 'rb')
-            audio_message = bot.send_audio(database_channel, audio)
-            # second send to user:
-            bot.send_audio(message.chat.id, audio_message.audio.file_id)
-#            os.remove(directory + file)
+            spotify_song_id = get_spotify_song_id(link)
+            existed_row = csv_search(spotify_song_id)
+            if existed_row:
+                telegram_audio_id = existed_row[2]
+                bot.send_audio(message.chat.id, telegram_audio_id)
+            else:
+                download(link)
+                # upload to telegram and delete from hard drive:
+                for file in file_list(directory): # we send every possible file in directory to bypass searching for file name
+                    # first send to database_channel:
+                    audio = open(directory + file, 'rb')
+                    audio_message = bot.send_audio(database_channel, audio)
+                    # add file to database
+                    csv_append(spotify_song_id, audio_message.audio.file_id)
+                    # second send to user:
+                    bot.send_audio(message.chat.id, audio_message.audio.file_id)
+                    # remove file from drive
+                    os.remove(directory + file)
     else:
-        log("No matches found. this line should not happen in normal behavior")
+        log("No matches found. this line should not happen in normal behavior, if happens is a bug.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
--- spotify.py
+++ spotify.py
@@ -1,22 +1,22 @@
 import re
 from variables import spotify_song_link_pattern, spotify_song_id_pattern
 
-def valid_song_link(text):
+def valid_spotify_song_link(text):
     return bool(re.match(spotify_song_link_pattern, text))
 
-def get_all_song_links(text):
+def get_all_spotify_song_links(text):
     matches = re.findall(spotify_song_link_pattern, text)
     return matches
 
-def get_song_id(song_link):
+def get_spotify_song_id(song_link):
     # use regex to extract the song ID from the link
     match = re.search(spotify_song_id_pattern, song_link)
     if match:
         # the song ID is captured in the first group of the regex match
-        song_id = match.group(1)
-        print(song_id)
+        spotify_song_id = match.group(1)
+        return spotify_song_id
     else:
-        print("No match found")
+        return False
 
 #######################################################################################
 """

2d89a050e3178abbfb9ab81a6fc51ba222138208
Arashnm80 <Arashnm80@gmail.com>
2023-08-02 05:10:47 +0000

    fix the "NoneType' object is not subscriptable" error

--- spotify.py
+++ spotify.py
@@ -20,6 +20,7 @@ def get_valid_spotify_links(text):
     regex_combined = re.compile("|".join(regexes))
     # Find all matches and store them in a list
     all_matches = [match.group() for match in regex_combined.finditer(text)]
+    print(all_matches) # as debug
     return all_matches
 
 def get_track_ids(link):
@@ -38,13 +39,22 @@ def get_track_ids(link):
         tracks = sp.album_tracks(link_id)["items"]
         track_ids = [t["id"] for t in tracks]
     elif link_type == "playlist":
-        # handle spotify results paginated in 100 items - https://stackoverflow.com/questions/39086287/spotipy-how-to-read-more-than-100-tracks-from-a-playlist
+        # handle spotify results paginated in 100 items 
+        # https://stackoverflow.com/questions/39086287/spotipy-how-to-read-more-than-100-tracks-from-a-playlist
         results = sp.playlist_tracks(link_id)
         tracks = results['items']
         while results['next']:
             results = sp.next(results)
             tracks.extend(results['items'])
-        track_ids = [t["track"]["id"] for t in tracks]
+        track_ids = []
+        for t in tracks:
+            if ("track" in t) and (t["track"] is not None) and ("id" in t["track"]) and (t["track"]["id"] is not None):
+                # due to a bug, a small number of tracks in playlists don't have
+                # ["track"]["id"] and cause the program to crash
+                # like 75th track in https://open.spotify.com/playlist/64r1Ry0JIWHboowR4LWp5R
+                # which is https://open.spotify.com/track/46cdw28EXOhDPnD1emDC6T
+                # so we check and make sure this field exists
+                track_ids.append(t["track"]["id"])
     else:
         return []
 

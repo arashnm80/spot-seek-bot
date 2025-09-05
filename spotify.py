dc3bce0233e24f380be2dbfeb647e62626372220
Arash Nemat Zadeh <arashnm80@gmail.com>
2025-09-05 02:56:41 +0000

    dry principle

--- spotify.py
+++ spotify.py
@@ -5,7 +5,28 @@ import requests # for get_redirect_link
 import random
 from spotipy_anon import SpotifyAnon
 from variables import *
-from csv_functions import get_telegram_audio_id
+from db_functions import get_telegram_audio_id
+from log import *
+import time
+
+# make parameters None to not use them
+def create_spotipy_instance(requests_session=warp_session, auth_manager=SpotifyAnon()):
+    # random spotify app from list to avoid rate limiting
+    random.seed(time.time())
+    spotify_app = random.choice(spotify_apps_list)
+    spotify_client_id = spotify_app[0]
+    spotify_client_secret = spotify_app[1]
+    # Authentication - without user
+    client_credentials_manager = SpotifyClientCredentials(client_id=spotify_client_id,
+                                                          client_secret=spotify_client_secret)
+    # spotipy instance
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager,
+                         requests_session=requests_session,
+                         auth_manager=auth_manager)
+    return sp
+    # fixme: looks like spotipy.Spitify requests_session has True/False values, not a session object.
+    # fixme: update: despite above sentence, looks like that's the correct way to use it.
+    # there is another `proxies` parameter in it. check it out.
 
 def get_redirect_link(shortened_link):
     response = requests.head(shortened_link, allow_redirects=True)
@@ -33,16 +54,7 @@ def get_valid_spotify_links(text):
     print(all_matches) # as debug
     return all_matches
 
-def get_track_ids(link):
-    # experimental new multiple spotify app system
-    spotify_app = random.choice(spotify_apps_list)
-    spotify_client_id = spotify_app[0]
-    spotify_client_secret = spotify_app[1]
-    #Authentication - without user
-    client_credentials_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
-    # spotipy instance with warp_session and spotifyAnon - remove auth_manager to use without SpotifyAnon
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, requests_session=warp_session, auth_manager=SpotifyAnon())
-    
+def get_track_ids(link):    
     # get id of link, album or playlist
     link_id = link.split("/")[-1].split("?")[0]
 
@@ -51,12 +63,22 @@ def get_track_ids(link):
         # extract track id directly from link without api
         track_ids = [link_id]
     elif link_type == "album":
+        sp = create_spotipy_instance(auth_manager=None)
         tracks = sp.album_tracks(link_id)["items"]
         track_ids = [t["id"] for t in tracks]
     elif link_type == "playlist":
+        sp = create_spotipy_instance(auth_manager=None)
+
+        try:
+            results = sp.playlist_tracks(link_id)
+        except Exception as e:
+            log(f"🛑 error in getting playlist tracks:\n\n{e}\n\n\ntrying again with spotipyAnon:")
+            sp = create_spotipy_instance()
+            results = sp.playlist_tracks(link_id)
+            log("spotifyAnon worked fine✅")
+
         # handle spotify results paginated in 100 items 
         # https://stackoverflow.com/questions/39086287/spotipy-how-to-read-more-than-100-tracks-from-a-playlist
-        results = sp.playlist_tracks(link_id)
         tracks = results['items']
         while results['next']:
             results = sp.next(results)
@@ -78,17 +100,8 @@ def get_track_ids(link):
 
     return(track_ids)
 
-def get_track_image(track_link):
-    # experimental new multiple spotify app system
-    spotify_app = random.choice(spotify_apps_list)
-    spotify_client_id = spotify_app[0]
-    spotify_client_secret = spotify_app[1]
-
-    #Authentication - without user
-    client_credentials_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
-    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager, requests_session=warp_session)
-    
-    track_id = track_link.split("/")[-1].split("?")[0]
+def get_track_image(track_id):
+    sp = create_spotipy_instance(auth_manager=None)
     track = sp.track(track_id)
     cover_image_url = track['album']['images'][0]['url']
     
@@ -96,17 +109,9 @@ def get_track_image(track_link):
 
 # search for a track name in spotify and return their track ids
 def search_track_ids(query):
-# experimental new multiple spotify app system
-    spotify_app = random.choice(spotify_apps_list)
-    spotify_client_id = spotify_app[0]
-    spotify_client_secret = spotify_app[1]
-    #Authentication - without user
-    client_credentials_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
-    # spotipy instance with warp_session and spotifyAnon - remove auth_manager to use without SpotifyAnon
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, requests_session=warp_session, auth_manager=SpotifyAnon())
-    
+    sp = create_spotipy_instance(auth_manager=None)
     # Perform a search for tracks using the provided query
-    results = sp.search(q=query, type='track', limit=5)
+    results = sp.search(q=query, type='track', limit=10)
     
     # Extract and return the relevant track information
     tracks = []

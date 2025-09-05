927a28bdc9b15441ba3dbc33312137107618bcd7
Arash Nemat Zadeh <arashnm80@gmail.com>
2025-09-05 02:55:03 +0000

    minor modification

--- delete_tracks_from_db.py
+++ delete_tracks_from_db.py
@@ -1,4 +1,4 @@
-from csv_functions import delete_track
+from db_functions import delete_track
 from spotify import get_track_ids
 
 link_to_delete = "https://open.spotify.com/track/xxxxxxxxxxxxxxxxxx"
